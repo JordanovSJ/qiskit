55282d331629dca194068102cfe22d4c765ef5c6
Jordan Jordanov <jordanovsj@gmail.com>
2018-12-07 12:58:20 +0000

    add CCCU1

--- Anhert_Payne/multi_qubit_CNOT.py
+++ Anhert_Payne/multi_qubit_CNOT.py
@@ -1,6 +1,7 @@
 import qiskit
-# from qiskit.tools.visualization import plot_histogram
+from qiskit.tools.visualization import plot_histogram
 import numpy as np
+# import matplotlib.pyplot as plt
 
 
 # works
@@ -13,44 +14,51 @@ def toffoli_gate(q_circ, ctrl_q1, ctrl_q2, target_q):
 
 
 def CCU1(axis, angle, q_circ, ctrl1, ctrl2, target):
-    """ Adds components acting as a doubly controlled rotation gate to an existing
+    """ Add gates acting as a doubly controlled rotation gate to an existing
         quantum circuit.
         Valid values for axis: 'y' and 'z' (any rotation can be specified by Y and
-        Z rotations only).
-        """
+        Z rotations only, no need for X).
+    """
     if axis == 'y':
-        q_circ.u3(angle / 2, 0, 0, target)
-        q_circ.ccx(ctrl1, ctrl2, target)
-        q_circ.u3(-angle / 2, 0, 0, target)
-        q_circ.ccx(ctrl1, ctrl2, target)
+        q_circ.cu3(angle / 2, 0, 0, ctrl1, target)
+        q_circ.cx(ctrl2, target)
+        q_circ.cu3(-angle / 2, 0, 0, ctrl1, target)
+        q_circ.cx(ctrl2, target)
     elif axis == 'z':
-        q_circ.u3(0, 0, angle / 2, target)
-        q_circ.ccx(ctrl1, ctrl2, target)
-        q_circ.u3(0, 0, -angle / 2, target)
-        q_circ.ccx(ctrl1, ctrl2, target)
+        q_circ.cu3(0, 0, angle / 2, ctrl1, target)
+        q_circ.cx(ctrl2, target)
+        q_circ.cu3(0, 0, -angle / 2, ctrl1, target)
+        q_circ.cx(ctrl2, target)
     else:
         raise ValueError('Rotational axis not valid')
 
 
 def CCCU1(axis, angle, q_circ, ctrl1, ctrl2, ctrl3, target):
-
-    # TODO
-    return 0
+    """ Add gates acting as a triple controlled rotation gate, to an existing
+        quantum circuit.
+        Valid values for axis: 'y' and 'z' (any rotation can be specified by Y and
+        Z rotations only, no need for X).
+    """
+    CCU1(axis, angle/2, q_circ, ctrl1, ctrl2, target)
+    q_circ.cx(ctrl3, target)
+    CCU1(axis, -angle / 2, q_circ, ctrl1, ctrl2, target)
+    q_circ.cx(ctrl3, target)
 
 
 if __name__ == "__main__":
-    q = qiskit.QuantumRegister(3, 'q')
+    q = qiskit.QuantumRegister(4, 'q')
     qc = qiskit.QuantumCircuit(q)
 
     qc.h(q[0])
     qc.h(q[1])
+    qc.h(q[2])
 
-    CCU1('y', np.pi, qc, q[0], q[1], q[2])
+    CCCU1('y', np.pi/2, qc, q[0], q[1], q[2], q[3])
 
     # Add Measurements
 
     # Create a Classical Register with 3 bits.
-    c = qiskit.ClassicalRegister(3, 'c')
+    c = qiskit.ClassicalRegister(4, 'c')
     # Create a Quantum Circuit
     meas = qiskit.QuantumCircuit(q, c)
     meas.barrier(q)
@@ -72,6 +80,6 @@ if __name__ == "__main__":
     result = job.result()
 
     counts = result.get_counts(qc)
-    print(counts)
-    # plot_histogram(counts)
+
+    plot_histogram(counts)
 
